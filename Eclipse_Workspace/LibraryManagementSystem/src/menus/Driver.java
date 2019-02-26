ca9d100fc0d6f145e44fa6db16be871992b06a12
Matt Allwright <matt.a.4890@gmail.com>
2019-02-25 18:49:36 -0700

    Moved loose files into eclipse workspace with package

---
+++ Eclipse_Workspace/LibraryManagementSystem/src/menus/Driver.java
@@ -0,0 +1,12 @@
+package menus;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		LoginMenu loginMenu = new LoginMenu();
+		loginMenu.runMenu();
+		
+	}
+
+}
---
+++ Eclipse_Workspace/LibraryManagementSystem/src/menus/MenuInput.java
@@ -0,0 +1,41 @@
+package menus;
+
+import java.util.Scanner;
+
+public class MenuInput {
+
+	private int maxChoice;
+	
+	public MenuInput(int maxChoice) {
+		this.maxChoice = maxChoice;
+	}
+	
+	public int getInput() {
+		
+		Scanner in = new Scanner(System.in);
+		boolean isValid = false;
+		int choice = 3;
+		
+		while (!isValid) {
+			try{
+				System.out.print("Please make selection: ");
+				choice = Integer.parseInt(in.nextLine());
+				
+				if (choice < 0 || choice > maxChoice) {
+					System.out.println("Error: Selection out of range");
+				}
+				else {
+					isValid = true;
+				}
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Error: Invalid selection");
+			}
+		}
+		
+		in.close();
+		
+		return choice;
+	}
+	
+}
